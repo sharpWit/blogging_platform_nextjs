7afbb854e2489c329712a70e8bba09bf6522ca79
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2024-09-12 15:58:58 +0330

    feat(auth): implemented auth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        "13": "repeat(13, minmax(0, 1fr))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          400: "#2589FE",
+          500: "#0070F3",
+          600: "#2F6FEB",
+        },
+      },
+    },
+    keyframes: {
+      shimmer: {
+        "100%": {
+          transform: "translateX(100%)",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
